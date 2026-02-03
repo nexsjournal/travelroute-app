198739ab3ac29512379f15ee6c4338518f394e5a
lex <lextellsyou@gmail.com>
2026-02-03 19:03:23 +0800

    Initial Commit

---
+++ travelroute/travelrouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  travelrouteApp.swift
+//  travelroute
+//
+//  Created by LexRay on 2026/2/3.
+//
+
+import SwiftUI
+
+@main
+struct travelrouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
